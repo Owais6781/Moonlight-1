20dddfc3903d6d0f40a82d7d162c71446a2f07aa
Owais6781 <owaisahmed6781@gmail.com>
2026-01-29 12:28:38 +0530

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+ import tailwindcss from '@tailwindcss/vite'
 
 
-export default defineConfig({
+ export default defineConfig({
   plugins: [
     
         react(),
-       tailwindcss(),
-      
+      tailwindcss(),
   ],
-})
+ build: {
+    outDir: "dist",     // Match wrangler.toml
+    rollupOptions: {
+      input: "index.html"
+    }
+  }
+ })
+
+
