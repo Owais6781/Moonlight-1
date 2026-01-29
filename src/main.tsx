b6a1cfbf3f4ac544fe59468cba7697d92aefed30
Owais6781 <owaisahmed6781@gmail.com>
2026-01-29 12:08:56 +0530

    update

--- src/main.tsx
+++ src/main.tsx
@@ -1,22 +1,11 @@
-// import { StrictMode } from 'react'
-// import { createRoot } from 'react-dom/client'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
 
-// import './index.css'
-// import App from './App.tsx'
+import './index.css'
+import App from './App.tsx'
 
-// createRoot(document.getElementById('root')!).render(
-//   <StrictMode>
-//     <App />
-//   </StrictMode>,
-// )
-
-
-import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import App from "./App";
-
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <BrowserRouter>
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
     <App />
-  </BrowserRouter>
-);
+  </StrictMode>,
+)
